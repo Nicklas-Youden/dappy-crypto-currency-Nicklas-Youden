a99b5acba78aedddc073ed7f1c1e03bebe3edbf7
Nicklas Hansen <you@example.com>
2021-04-13 11:27:37 +0200

    Hotfix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ export default function Hero(){
         Create your first decentrlized wallet
       </h1>
       <p className="saleText__subTitle">The easiest way to mange multiple cryptocurrenct assets</p>
-      <a className="saleText__callToAction" href="!"
+      <a className="saleText__callToAction" href="/"
       // eslint-disable-next-line
       onClick={() => gtag("event", "select_content", { "content_type": createWalletButton, "test-type": "A" })}
       >create wallet</a>
